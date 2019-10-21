ad3e6b54b5f99040a00f9f72925a2fa909c61b8b
Victor Nguyenta <s3546430@student.rmit.edu.au>
2019-10-21 17:04:22 +1100

    navbar

--- cloudcomputing2.py
+++ cloudcomputing2.py
@@ -19,7 +19,7 @@ def login():
 
 @app.route('/upload_image')
 def upload_image():
-    return render_template('Upload_Image.html')
+    return render_template('Application.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
